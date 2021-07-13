5f3364fe55c293f54d108146315772393ea5ce6e
Mackling <mackhoustt@gmail.com>
2021-07-13 10:52:19 -0500

    9 commit

---
+++ src/components/lista.jsx
@@ -0,0 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const CarList = ({...}) => {
+
+    const cars = useMemo(() => 
+    ...(...),
+    [...]);
+  
+      return (
+        <section>
+
+        <div class="d-flex justify-content-start">
+        {
+                 ....map(... => (
+                      <... key={....id}
+                      {...} />
+                  ))
+              }
+            </div>
+        </section>
+       
+        
+      )
+  }
+
+return(
+)
