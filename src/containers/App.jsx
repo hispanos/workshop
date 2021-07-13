f92b34c77ef1520305777ec2aeb79d0d5c246f98
lfernanda2504 <luizafernandagarcia@gmail.com>
2021-07-13 12:12:17 -0500

    componente editar

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Video from '../components/Video'
+import EditMotocycle from '../components/EditMotocycle';
 
 const App = () => {
     return (
         <div>
             <Video />
+            <EditMotocycle/>
         </div>
     )
 }
