e0ae1b72105c37cdf994b8e960337da52e18e622
lfernanda2504 <luizafernandagarcia@gmail.com>
2021-07-13 10:21:59 -0500

    agregando menu

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
     return (
         <div>
-            Hola
+            <NavBar/>
         </div>
     )
 }
